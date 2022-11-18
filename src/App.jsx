83d115076bdd9cc11c22314a483cbbcaa8b550dc
christy313 <christycreativity@gmail.com>
2022-11-17 20:11:55 -0500

    Set up routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "../src/pages/Home";
+import ProductList from "../src/pages/ProductsList";
+import SingleProduct from "../src/pages/SingleProduct";
 import Cart from "../src/pages/Cart";
+import Login from "../src/pages/Login";
+import Register from "../src/pages/Register";
 
 const App = () => {
-  return <Cart />;
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home />}></Route>
+        <Route path="/products/:category" element={<ProductList />}></Route>
+        <Route path="/product/:id" element={<SingleProduct />}></Route>
+        <Route path="/cart/" element={<Cart />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/register" element={<Register />}></Route>
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
