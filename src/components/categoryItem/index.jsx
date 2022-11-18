77b1682ccade9873d1903ea5acddebb94eac5fef
christy313 <christycreativity@gmail.com>
2022-11-18 17:06:05 -0500

    Finish filter products

--- src/components/categoryItem/index.jsx
+++ src/components/categoryItem/index.jsx
@@ -1,14 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { Container, Image, Info, Title, Button } from "./CategoryItem.styles";
 
 const CategoryItem = ({ item }) => {
   return (
     <Container>
-      <Image src={item.img} />
-      <Info>
-        <Title>{item.title}</Title>
-        <Button>SHOP NOW</Button>
-      </Info>
+      <Link to={`/products/${item.category}`}>
+        <Image src={item.img} />
+        <Info>
+          <Title>{item.title}</Title>
+          <Button>SHOP NOW</Button>
+        </Info>
+      </Link>
     </Container>
   );
 };
