bbf4b4ac9bfab3f65e7cc2c06578da375eb83b2e
christy313 <christycreativity@gmail.com>
2022-11-19 15:59:44 -0500

    Install stripe

--- src/pages/Cart/index.jsx
+++ src/pages/Cart/index.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import StripeCheckout from "react-stripe-checkout";
 import { useSelector } from "react-redux";
 import Notice from "../../components/Notice";
 import Navbar from "../../components/Navbar";
@@ -37,6 +38,8 @@ import {
   SummaryItemPrice,
   Button,
 } from "./Cart.styles";
+
+const stripeKey = process.env.REACT_APP_STRIPE;
 
 const Cart = () => {
   const cart = useSelector((state) => state.cart);
@@ -105,7 +108,17 @@ const Cart = () => {
               <SummaryItemText>Total</SummaryItemText>
               <SummaryItemPrice>$ {cart.total}</SummaryItemPrice>
             </SummaryItem>
-            <Button>CHECKOUT NOW</Button>
+
+            <StripeCheckout
+              name="gap"
+              // image="https://avatars.githubusercontent.com/u/1486366?v=4"
+              billingAddress
+              shippingAddress
+              description={`Your total is $${cart.total}`}
+              amount={cart.total * 100}
+              // token={onToken}
+              stripeKey={stripeKey}
+            />
           </Summary>
         </Bottom>
       </Wrapper>
