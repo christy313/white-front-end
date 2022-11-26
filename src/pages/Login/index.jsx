f6c954deecf3f53dcc6c5ec035accfcfaff0dfe4
christy313 <christycreativity@gmail.com>
2022-11-26 15:13:50 -0500

    Comment unused code to fix warning

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { login } from "../../redux/apiCalls";
 import Navbar from "../../components/Navbar/index";
