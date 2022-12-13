273cd1c7af181b5927ca97d410ca0c9ac3f16c15
christy313 <christycreativity@gmail.com>
2022-12-13 15:58:41 -0500

    Add disableReactDevTools

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import { store, persistor } from "./redux/store";
 import { PersistGate } from "redux-persist/integration/react";
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
+
+if (process.env.NODE_ENV === "production") disableReactDevTools();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
